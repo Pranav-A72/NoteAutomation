865a13762b3f84df5724fca7abb04f14eb63e23c
Boes0 <115201973+Boes0@users.noreply.github.com>
2022-11-20 09:52:54 -0600

    Add files via upload

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -1,3 +1,21 @@
+'''
+1.) isolate every "block" (edited)
+[10:00 PM]
+2.) isolate sentacnes/topics
+ex: Climate. What is a climate? Why is it important to agriculture?
+Into: [climate][what is climate][why is it important] (edited)
+[10:00 PM]
+3.) emlinate unless words
+ex: [why is important to agriculture] to [important, arguculture]
+[10:00 PM]
+4. find the first sentance with that phrase
+[10:01 PM]
+5.) elimate repeats
+ex: [climate][what is climate]
+to [climate][climate]
+[10:01 PM]
+to [climate]
+'''
 
 import tkinter as tk
 from tkinter import filedialog
@@ -8,21 +26,52 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 
 from PyPDF2 import PdfReader
-texting = '' 
+corneltext = '' 
 reader = PdfReader(file_path)
 pageNum = reader.getNumPages() - 1
 while pageNum > -1:
     page = reader.pages[pageNum]
-    texting = page.extract_text() + texting
+    corneltext = page.extract_text() + corneltext
     pageNum -= 1
 
 
-import textrazor
+# import textrazor
 
-textrazor.api_key = "fda84616d56c219481d317b308114baf4d94ce274a7dc5255bd86b91"
+# textrazor.api_key = "fda84616d56c219481d317b308114baf4d94ce274a7dc5255bd86b91"
 
-client = textrazor.TextRazor(extractors=["entities"])
-response = client.analyze(texting)
+# client = textrazor.TextRazor(extractors=["entities"])
+# response = client.analyze(texting)
 
-for entity in response.entities():
-    print(entity.id)
+# for entity in response.entities():
+#     print(entity.id)
+
+print(type(corneltext))
+cornel_splited_str = corneltext.splitlines(True)
+print(type(cornel_splited_str))
+
+#We need to save this so we can figure how many sentances to pull = space on page (lines breaks)
+print(cornel_splited_str)
+
+emptycounter = 0
+emptylimits = len(cornel_splited_str)
+empty_cornel = cornel_splited_str
+removed = 0
+print(emptylimits)
+# print(cornel_splited_str[emptycounter])
+while emptycounter < emptylimits :
+    if emptycounter >= len(empty_cornel):
+        emptycounter = emptylimits
+    else:
+        if cornel_splited_str[emptycounter] == ' \n' or cornel_splited_str[emptycounter] == '  \n' or cornel_splited_str[emptycounter] == '   \n':
+            remover = emptycounter + removed
+            empty_cornel.pop(remover)
+            # print(emptycounter)
+            # print(len(empty_cornel))
+            # print("a")
+        else:
+            emptycounter += 1
+            # print(emptycounter)
+            # print(len(empty_cornel))
+            # print("b")
+print(empty_cornel)
+print(len(empty_cornel))
