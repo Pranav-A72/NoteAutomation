23a8f87c0d899276585ff55f0257843dce30557c
Pranav Anuraag <114312980+Pranav-A72@users.noreply.github.com>
2022-11-21 09:15:23 -0600

    jake

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-// main file first
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+
+#open text file in read mode
+text_file = open(file_path, "r")
+ 
+#read whole file to a string
+data = text_file.read()
+
+print(data)
