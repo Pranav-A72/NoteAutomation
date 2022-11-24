7918a93f2b696f13ce8fe0d81d62a46deada2f38
Pranav Anuraag <114312980+Pranav-A72@users.noreply.github.com>
2022-11-23 21:36:06 -0600

    Update pdf_to_txt.py

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -103,7 +103,7 @@ root.withdraw()
 
 file_path = filedialog.askopenfilename()
 
-with open(file_path, 'r') as f:
+with open(file_path, 'r', encoding='utf-8') as f:
     contents = f.read()
     # print(contents)
 
