1e9645da41a69f5d62755a12ec62a9cec56487a6
Pranav Anuraag <114312980+Pranav-A72@users.noreply.github.com>
2022-11-23 22:18:33 -0600

    Overlay Work _3

--- overlaytest.py
+++ overlaytest.py
@@ -11,10 +11,21 @@ pdf = FPDF()
 pdf.add_page()
 
 #Really Jake? I had to install a font named MyFont? I told you to change this
-pdf.set_font("Myfont", "", 20)
+pdf.set_font("arial", "", 20)
+
+pdf.set_xy(100, 50)
+
+with open("Output.txt") as f:
+    lines = f.readlines()
+
+text = ("").join(lines)
+
+pdf.multi_cell(w= 200, h=50, txt=text, align="L")
+
+pdf.output("testoverlay_out.pdf")
 
 
-
+"""
 packet = io.BytesIO()
 can = canvas.Canvas(packet, pagesize=letter)
 can.drawString(10, 100, "Hello world")
@@ -36,3 +47,5 @@ output.addPage(page)
 outputStream = open("destination.pdf", "wb")
 output.write(outputStream)
 outputStream.close()
+
+"""
