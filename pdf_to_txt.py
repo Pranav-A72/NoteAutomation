b9186e9455bcc118c340290cd54ab5931a61c98f
Boes0 <115201973+Boes0@users.noreply.github.com>
2022-11-29 06:49:41 -0600

    Add files via upload

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -1,6 +1,44 @@
 import tkinter as tk
 from tkinter import filedialog
 import requests
+from reportlab.pdfgen import canvas
+import io
+from reportlab.lib.pagesizes import letter
+packet = io.BytesIO()
+import random
+from PyPDF2 import PdfFileWriter, PdfFileReader
+new_page_check = 0
+counter_char = 0
+can = canvas.Canvas(packet, pagesize=letter)
+ycount = 0
+intentran = random.randrange(10, 17)
+x = 185 + intentran
+import math
+pchecker = 0
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+pdfmetrics.registerFont(TTFont('TestFont0', 'Myfont-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont1', 'Jakehand1-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont2', 'Jakehand2-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont3', 'Boesjake3-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont4', 'Boesjake4-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont5', 'Boesjake5-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont6', 'Boesjake6-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TestFont7', 'Boesjake7-Regular.ttf'))
+
+pagecounter = 0
+
+fonts = [ 'TestFont0', 'TestFont1', 'TestFont2', 'TestFont3', 'TestFont4', 'TestFont5', 'TestFont6', 'TestFont7']
+dontsize = random.randrange(190, 210)/10
+fontnum = random.randrange(1, 7)
+string_counter = 0
+def setcolor(can):
+    can.setFillColorRGB(0, 0.1, 0.5)
+can.line(2,2,2, 2)
+jumper = 524
+
 
 root = tk.Tk()
 root.withdraw()
@@ -63,7 +101,7 @@ empty_counter2 = 0
 edit_emptycornel = empty_cornel
 # print(edit_emptycornel[2])
 
-redundent_words = [' the ', ' that ', ' it ', ' than ', 'Notes', 'Cornell', 'Questions', ' does ', ' or ', ' from ', ' to ', ' and ', "(", ")", ".", ",", "?", " is ", " important", "Study", "Describe", " are ", " of ", " Date ", " Name ", ":", " Class ", "/", " Period ", "5", "6", "7", "8", "9", "0", ' as', ' by', ' in ', "As ", '\'', " do", " most ", " we", " a ", "1", "2", "3", "4", " world ", " level ", " regard ", " people " " v ", " vs ", " role ", " at ", "Look ", " look ", "Why ", "How ", "What ", "Who ", "Where ", ' why ', " what ", " how ", ' who ', ' when ', ' where ', '’s', 'v.', "v ", " high ", " low", "with ", " see ", " per ", " used ", " factors ", " money ", " in ", " countries ", "theredifference", "measurecountry", " population ", " work ", " collected"]
+redundent_words = [' the ', ' that ', ' for ', ' two ', ' an ', ' it ', ' than ', 'Notes', 'Cornell', 'Questions', ' does ', ' or ', ' from ', ' to ', ' and ', "(", ")", ".", ",", "?", " is ", " important", "Study", "Describe", " are ", " of ", " Date ", " Name ", ":", " Class ", "/", " Period ", "5", "6", "7", "8", "9", "0", ' as', ' by', ' in ', "As ", '\'', " do", " most ", " we", " a ", "1", "2", "3", "4", " world ", " level ", " regard ", " people " " v ", " vs ", " role ", " at ", "Look ", " look ", "Why ", "How ", "What ", "Who ", "Where ", ' why ', " what ", " how ", ' who ', ' when ', ' where ', '’s', 'v.', "v ", " high ", " low", "with ", " see ", " per ", " used ", " factors ", " money ", " in ", " countries ", "theredifference", "measurecountry", " population ", " work ", " collected", " but ", " about ", " benefit ", " value "]
 empty_counter = 0
 emptylimits = len(redundent_words)
 
@@ -106,6 +144,7 @@ while counter58 < len(_saver):
     counter58 += 1
 
 print(linebreaks)
+
 
 import tkinter as tk
 from tkinter import filedialog
@@ -175,25 +214,52 @@ while number < len(edit_emptycornel) - 1:
     
     # print(block)
     # print(len(paratext) - 1)
-    # print(sentancestaken)
+    print(sentancestaken)
+
+
+
 
     counter_repeat = 0
     counter_remove = 0
+    copysentance = sentancestaken.copy()
     while counter_repeat < len(paratext) - 1:
         useage = sentancestaken.count(counter_repeat)
-        if 0 < useage and useage < (round(len(morewords_cornel) - add_check) / 2):
-            while counter_remove < useage:
-                sentancestaken.remove(counter_repeat)
-                counter_remove += 1
-            counter_remove = 0
-        counter_repeat += 1
-    
+        if len(morewords_cornel) == 1:
+            if 0 < useage and useage < (round(len(morewords_cornel) - add_check) / 2 ):
+                while counter_remove < useage:
+                    sentancestaken.remove(counter_repeat)
+                    counter_remove += 1
+                counter_remove = 0
+            counter_repeat += 1
+        else:
+            if 0 < useage and useage < (round(len(morewords_cornel) - add_check) / 2 + 1):
+                while counter_remove < useage:
+                    sentancestaken.remove(counter_repeat)
+                    counter_remove += 1
+                counter_remove = 0
+            counter_repeat += 1
+    counter_repeat = 0
+    subtractor = 2
+    if len(sentancestaken) == 0 and len(_saver[number]) > 50 and number != 2:
+        while counter_repeat < len(paratext) - 1:
+            useage = copysentance.count(counter_repeat)
+            if 0 < useage and useage < 6:
+                while counter_remove < useage:
+                    copysentance.remove(counter_repeat)
+                    counter_remove += 1
+                counter_remove = 0
+            counter_repeat += 1
+            
+            print(copysentance)
+          
+        sentancestaken = copysentance.copy()
+    print(sentancestaken)
     if ' \n' == _saver[number] or '  \n' == _saver[number] or '   \n' == _saver[number] or '  \n' == _saver[number]:
       zero = 0
     else:
-        if len(sentancestaken) < 1 and len(_saver[number]) > 2:
+        if len(sentancestaken) < 1 and len(_saver[number]) > 2 or "1. " in _saver[number] or "3. " in _saver[number] or "2. " in _saver[number] or "4. " in _saver[number] or "Name: " in _saver[number] or "page" in _saver[number] or "Case Study" in _saver[number] or "Reproductive Health and the GII" in _saver[number]:
             zero = 0 
-            summary = input("Answer")
+            finalsummary = input("Answer")
         else:
             sentancestaken.sort()
             twenty_six = sentancestaken.count(len(paratext) - 1)
@@ -205,11 +271,10 @@ while number < len(edit_emptycornel) - 1:
             # print(sentancestaken)
             counter_outlie = 0
             while counter_outlie < len(sentancestaken):
-                if sentancestaken[counter_outlie] > sentancestaken[3] + 7:
+                if sentancestaken[counter_outlie] > sentancestaken[1] + 7:
                     sentancestaken.pop(counter_outlie)
                 else:
                     counter_outlie += 1
-        
             if len(sentancestaken) < 1:
                 zero = 0
             else:
@@ -232,15 +297,21 @@ while number < len(edit_emptycornel) - 1:
                 while counter_final < len(final_para):
                     block = block + " " + paratext[final_para[counter_final]]
                     counter_final += 1
-                # print(block)
+                print(block)
 
             #WIKI ARTICLE START
 
 
 
 
-            sentances = str.round(linebreaks[timesused] / 2)
-            timesused += 1
+            
+            if len(_saver[number]) < 300 and len(_saver[number]) > 221:
+                sentances = str(round(linebreaks[timesused] / 8))
+            elif "Gender Disparities" in _saver[number]:
+                sentances = 3
+            else:
+                sentances = str(round(linebreaks[timesused] / 4))
+            
 
 
             url = "https://meaningcloud-summarization-v1.p.rapidapi.com/summarization-1.0"
@@ -255,20 +326,232 @@ while number < len(edit_emptycornel) - 1:
 
             response = requests.request("GET", url, headers=headers, params=querystring)
 
-            summary = response.text
+            summary = response.json()
+            print(summary)
+            countersplit = 0
+            split_summary = summary["summary"].split()
+            finalsummary = ""
+            andcounter = 0
+            print(split_summary[andcounter])
+            print(split_summary[andcounter].upper())
+            print(len(split_summary[andcounter]))
+            while split_summary[andcounter] == split_summary[andcounter].upper():
+                if len(split_summary[andcounter]) > 3:
+                    del split_summary[andcounter]
+                elif split_summary[andcounter] == 'AND':
+                    del split_summary[andcounter]
+                else:
+                    andcounter += 1
+                print(split_summary)
+            while countersplit < len(split_summary):
+            
+                finalsummary = finalsummary + " " + split_summary[countersplit]
+                countersplit += 1
+            print(summary["summary"])
+            print(finalsummary)
+        if "[...]" in finalsummary:
+            finalsummary = finalsummary.replace("[...]", '')
+        if "-" in finalsummary:
+            finalsummary = finalsummary.replace("-", "")
+        if "As you have read, t" in finalsummary:
+            finalsummary = finalsummary.replace("As you have read, t", "T")
+        if "1" in finalsummary:
+            finalsummary = finalsummary.replace("1", "")
+        if "2" in finalsummary:
+            finalsummary = finalsummary.replace("2", "")
+        if "3" in finalsummary:
+            finalsummary = finalsummary.replace("3", "")
+        if "4" in finalsummary:
+            finalsummary = finalsummary.replace("4", "")
+        if "5" in finalsummary:
+            finalsummary = finalsummary.replace("5", "")
+        if "6" in finalsummary:
+            finalsummary = finalsummary.replace("6", "")
+        if "7" in finalsummary:
+            finalsummary = finalsummary.replace("7", "")
+        if "8" in finalsummary:
+            finalsummary = finalsummary.replace("8", "")
+        if "9" in finalsummary:
+            finalsummary = finalsummary.replace("9", "")
+        if "0" in finalsummary:
+            finalsummary = finalsummary.replace("0", "")
+        if ".; . " in finalsummary:
+            finalsummary = finalsummary.replace("0", "")
+        if "m. t" in finalsummary:
+            finalsummary = finalsummary.replace("0", "m t")
 
 
         
-        if summary == " " or summary == "":
+        if finalsummary == " " or finalsummary == "" or number == 0 or number == 1:
             zero = 0
         else:
+            
+            
+            stringfinal = finalsummary
+            print(stringfinal)
+            characters = ([*stringfinal])
+            print(new_page_check)
+           
+                
+            
+            
+            if number != 4:
+                print(number)
+                if " Participation (" in _saver[number]:
+                    jumplines = (linebreaks[string_counter + 1])* 21.7 + 40
+                elif len(_saver[number]) < 83:
+                    jumplines = (linebreaks[string_counter + 1])* 21.7 + 21.7
+                    # jumplines = (linebreaks[string_counter + 2] - 1)* 22 + 55
+                elif len(_saver[number ]) < 160:
+                    
+                    jumplines = (linebreaks[string_counter + 1])* 21.7 + 28
+                elif len(_saver[number]) < 280:
+        
+                    jumplines = (linebreaks[string_counter + 1])* 21.7 + 37
+                elif len(_saver[number]) < 375:
+        
+                    jumplines = ((linebreaks[string_counter + 1])-5)* 21.7 + 70
+                else:
+                    jumplines = (linebreaks[string_counter + 1])* 21.7 + 77
+                
+                jumper = jumper - jumplines
+                print(jumper)
+            if (jumper) <= 20:
+                    can.showPage()
+                    pagecounter += 1
+                    ycount = 0
+                    jumper = 749
+                    new_page_check = 1
+                    if "Income (Gross Domestic Product" in _saver[number]:
+                        jumplines = 44 + 21.7
+                    elif len(_saver[number]) < 90:
+                        jumplines =  21.7 + 21.7
+                        # jumplines = (linebreaks[string_counter + 2] - 1)* 22 + 55
+                    elif len(_saver[number ]) < 160:
+                        
+                        jumplines =  28 + 21.7
+                    elif len(_saver[number]) < 280:
+                
+                        jumplines =  37 + 21.7
+                    elif len(_saver[number]) < 375:
+                        jumplines = 75 + 21.7 * 4
+                    else:
+                        jumplines = 77 + 21.7
+                    jumper = jumper - jumplines
+                    print(jumper)
+                    print(ycount)
+            
+            while counter_char < len(characters):
+                if "(" in characters[counter_char]:
+                    pchecker = 1
+                if ")" in characters[counter_char]:
+                    pchecker = 0
+                    counter_char += 1
+                if pchecker == 0:
+                    dontsize = random.randrange(245, 250)/10
+                    fontnum = str(random.randrange(0, 2))
+                    intentran = random.randrange(7, 15) # 20
+                    wordspacing2 = random.randrange(4, 20) # 33
+                    char_spacing2 = random.randrange(84, 87) / 10 #94
+                    charvar = random.randrange(10,  20) / 10
+                    wordspacing = random.randrange(280, 370) / 10
+                    
+                    can.setFont("TestFont" + fontnum, dontsize)
+                  
+                    can.drawString(x, (jumper + ycount * 21.7), characters[counter_char], wordSpace= wordspacing, charSpace= charvar)
+                
+                    if characters[counter_char] == " ":
+                        x = x + wordspacing2 + 3
+                    else:
+                        if characters[counter_char] == "W" or characters[counter_char] == "M" or characters[counter_char] == "m" or characters[counter_char] == "w" or characters[counter_char] == "I" or characters[counter_char] == "p" or characters[counter_char] == "D" or characters[counter_char] == "v" or characters[counter_char] == "k" or characters[counter_char] == "T":
+                            x = x + (len(characters[counter_char]) * (char_spacing2) + 4)
+                        else: 
+                            x = x + (len(characters[counter_char]) * char_spacing2)
+                    if x > 490 and characters[counter_char] == " ":
+                        x = 185 + intentran
+                        ycount += -1
+                counter_char += 1
+                
+                    # if "Income (Gross Domestic Product" in _saver[number]:
+                    #     jumplines =  44
+                    # elif len(_saver[number + 1]) < 92:
+                    #     jumplines =  21.7
+                    #     # jumplines = (linebreaks[string_counter + 2] - 1)* 22 + 55
+                    # elif len(_saver[number + 1]) < 160:
+                        
+                    #     jumplines =  28
+                    # elif len(_saver[number + 1]) < 280:
+            
+                    #     jumplines =  37
+                    # else:
+                    #     jumplines = 77
+                    
+                
+            ycount = 0
+            
+            x = 185
+            counter_char = 0
+            string_counter += 1
             print(_saver[number])
             print("")
-            print(summary)
+            print(finalsummary)
             print(" ")
             with open(file_path, 'a') as f:
-                f.write("\n" + _saver[number] + "\n\n" + summary + "\n\n\n")
+                f.write("\n" + _saver[number] + "\n\n" + finalsummary + "\n\n\n")
+        timesused += 1
     number +=1
         
+can.save()
+#move to the beginning of the StringIO buffer
+packet.seek(0)
+# create a new PDF with Reportlab
+new_pdf = PdfFileReader(packet)
+# read your existing PDF
+existing_pdf = PdfFileReader(open('current.pdf', "rb"))
+output = PdfFileWriter()
+# add the "watermark" (which is the new pdf) on the existing page
+pgcounter2 = 0
+while pgcounter2 < pagecounter + 1:
+    page = existing_pdf.getPage(pgcounter2)
+    page.mergePage(new_pdf.getPage(pgcounter2))
+    output.addPage(page)
+    pgcounter2 += 1
+# finally, write "output" to a real file
+outputStream = open("destination.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
+print(linebreaks)
 
+#positioning, pdf overlay
+"""
+import fpdf 
+from fpdf import FPDF
 
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+packet = io.BytesIO()
+can = canvas.Canvas(packet, pagesize=letter)
+can.drawString(10, 100, "Hello world")
+can.save()
+
+#move to the beginning of the StringIO buffer
+packet.seek(0)
+
+# create a new PDF with Reportlab
+new_pdf = PdfFileReader(packet)
+# read your existing PDF
+existing_pdf = PdfFileReader(open("original.pdf", "rb"))
+output = PdfFileWriter()
+# add the "watermark" (which is the new pdf) on the existing page
+page = existing_pdf.getPage(0)
+page.mergePage(new_pdf.getPage(0))
+output.addPage(page)
+# finally, write "output" to a real file
+outputStream = open("destination.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
+
+"""
--- playground.py
+++ playground.py
@@ -6,7 +6,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 packet = io.BytesIO()
 import random
-string0 = "Development in the general sense is the process of change in the nature and activities of the economy of a region or country and subsequent increases."
+string0 = "Test"
 string1 = "GDP and GNP are ways of measuring economic growth within a country. Formal and inform sectors are things. Informal is like government and stuff anf formal is another thing that relates to sectors."
 string2 = 'In addition to classifying economic sectors, the structure of an economy can also be broken into two categories: the formal sector and the informal sector. The formal sector includes businesses, enterprises, and other economic activities that have government supervision, monitoring, and protection, and are taxed.'
 lines = [5, 9 , 9]
@@ -17,70 +17,71 @@ counter_char = 0
 can = canvas.Canvas(packet, pagesize=letter)
 ycount = 0
 intentran = random.randrange(10, 17)
-x = 185 + intentran
+x = 190
 
 
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 pdfmetrics.registerFont(TTFont('TestFont0', 'Myfont-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont1', 'Jakehand1-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont2', 'Jakehand2-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont3', 'Boesjake3-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont4', 'Boesjake4-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont5', 'Boesjake5-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont6', 'Boesjake6-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TestFont7', 'Boesjake7-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont1', 'Jakehand1-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont2', 'Jakehand2-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont3', 'Boesjake3-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont4', 'Boesjake4-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont5', 'Boesjake5-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont6', 'Boesjake6-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TestFont7', 'Boesjake7-Regular.ttf'))
 
-fonts = ['TestFont', 'TestFont1', 'TestFont2', 'TestFont3', 'TestFont4', 'TestFont5', 'TestFont6', 'TestFont7']
+# fonts = ['TestFont', 'TestFont1', 'TestFont2', 'TestFont3', 'TestFont4', 'TestFont5', 'TestFont6', 'TestFont7']
 dontsize = random.randrange(190, 210)/10
 fontnum = random.randrange(0, 7)
 string_counter = 0
+
 def setcolor(can):
     can.setFillColorRGB(0, 0.1, 0.5)
 can.line(2,2,2, 2)
-jumper = 530
+jumper = 606
 setcolor(can)
-while string_counter < len(lines):
-    print(string_counter)
-    stringfinal = strings[string_counter]
-    print(stringfinal)
-    characters = ([*stringfinal])
-    print(string_counter)
-    if string_counter == 1:
-        jumplines = (lines[string_counter] - 1)* 22 + 55
+# while string_counter < len(lines):
+print(string_counter)
+stringfinal = strings[string_counter]
+print(stringfinal)
+characters = ([*stringfinal])
+print(string_counter)
+can.showPage()
+jumplines = (lines[string_counter] - 1)* 22
+while counter_char < len(characters):
+    
+    dontsize = random.randrange(245, 250)/10
+    fontnum = str(random.randrange(0, 2))
+    intentran = random.randrange(7, 20)
+    wordspacing2 = random.randrange(4, 33)
+    char_spacing2 = random.randrange(84, 92) / 10
+    charvar = random.randrange(10,  20) / 10
+    wordspacing = random.randrange(280, 370) / 10
+    def setcolor(can):
+        can.drawString(x, jumper + ycount * 21.7, characters[counter_char], wordSpace= 10, charSpace= 1.5)
+    setcolor(can)
+    if characters[counter_char] == " ":
+        x = x + wordspacing2 + 3
     else:
-        jumplines = (lines[string_counter] - 1)* 22 + 63
-    while counter_char < len(characters):
-        
-        dontsize = random.randrange(245, 250)/10
-        fontnum = str(random.randrange(0, 2))
-        intentran = random.randrange(7, 20)
-        wordspacing2 = random.randrange(1, 21)
-        char_spacing2 = random.randrange(84, 95) / 10
-        charvar = random.randrange(10,  20) / 10
-        wordspacing = random.randrange(280, 370) / 10
-        def setcolor(can):
-            can.setFont("TestFont" + fontnum, dontsize)
-            can.setFillColorCMYK(.31, .41, 0.0, .66)
-            can.drawString(x, 50 + (jumper + ycount * 22), characters[counter_char], wordSpace= wordspacing, charSpace= charvar)
-        setcolor(can)
-        if characters[counter_char] == " ":
-            x = x + wordspacing2 + (len(characters[counter_char]) * 7)
-        else:
-            if characters[counter_char] == "m" or characters[counter_char] == "w" or characters[counter_char] == "I" or characters[counter_char] == "p":
-                x = x + (len(characters[counter_char]) * (char_spacing2) + 5)
-            else: 
-                x = x + (len(characters[counter_char]) * char_spacing2)
-        if x > 500 and characters[counter_char] == " ":
-            x = 185 + intentran
-            ycount += -1
-        counter_char += 1
-    ycount = 0
-    jumper = jumper - jumplines
-    x = 185
-    string_counter += 1
-    counter_char = 0
+        if characters[counter_char] == "m" or characters[counter_char] == "w" or characters[counter_char] == "I" or characters[counter_char] == "p" or characters[counter_char] == "D" or characters[counter_char] == "v" or characters[counter_char] == "k" or characters[counter_char] == "T":
+            x = x + (len(characters[counter_char]) * (char_spacing2) + 5)
+        else: 
+            x = x + (len(characters[counter_char]) * char_spacing2)
+    if x > 500 and characters[counter_char] == " ":
+        x = 185 + intentran
+        ycount += -1
+    counter_char += 1
+    if (jumper + ycount * 21.7) <= 0:
+        can.showPage()
+        ycount = 0
+        jumper = 580
+ycount = 0
+jumper = jumper - jumplines
+x = 185
+string_counter += 1
+counter_char = 0
 
 can.save()
 #move to the beginning of the StringIO buffer
@@ -88,12 +89,15 @@ packet.seek(0)
 # create a new PDF with Reportlab
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
-existing_pdf = PdfFileReader(open('one9.1Notes.pdf', "rb"))
+existing_pdf = PdfFileReader(open('current.pdf', "rb"))
 output = PdfFileWriter()
 # add the "watermark" (which is the new pdf) on the existing page
-page = existing_pdf.getPage(0)
-page.mergePage(new_pdf.getPage(0))
+page = existing_pdf.getPage(1)
+page.mergePage(new_pdf.getPage(1))
 output.addPage(page)
+# page = existing_pdf.getPage(1)
+# page.mergePage(new_pdf.getPage(1))
+# output.addPage(page)
 # finally, write "output" to a real file
 outputStream = open("destination.pdf", "wb")
 output.write(outputStream)
